504041b9b8c9aaaaafa763cce57f473779e27e9b
MD MERAJ <mmd200975@gmail.com>
2024-03-08 13:50:02 +0530

    Successfully Commit

--- ClassAndObject/src/Demo.java
+++ ClassAndObject/src/Demo.java
@@ -1,16 +1,18 @@
+// Define a class Test with integer and string attributes.
 class Test {
-    int a = 1;
-    String b = "MERAJ";
+    int a = 1; // Declare and initialize an integer variable.
+    String b = "MERAJ"; // Declare and initialize a string variable.
 
-    void show()
-    {
-        System.out.println(a+" "+b);
+    // Method to display the values of integer and string attributes.
+    void show() {
+        System.out.println(a + " " + b); // Print the values of attributes.
     }
 }
+
+// Main class Demo to demonstrate the usage of the Test class.
 class Demo {
     public static void main(String[] args) {
-        Test r = new Test();
-        r.show();
-
+        Test r = new Test(); // Create an instance of the Test class.
+        r.show(); // Call the show method to display the values.
     }
 }
--- ClassAndObject/src/Myclass.java
+++ ClassAndObject/src/Myclass.java
@@ -1,24 +1,28 @@
-class Fruit{
-    String name;
-    String color;
-    
+// Define a class Fruit to represent fruits with name and color attributes.
+class Fruit {
+    String name; // Declare variable to store fruit name.
+    String color; // Declare variable to store fruit color.
+
+    // Method to display the name and color of the fruit.
     void show() {
-        System.out.println("Fruits name is ="+name);
-        System.out.println("Fruits color is ="+color);
+        System.out.println("Fruit's name is = " + name); // Print fruit name.
+        System.out.println("Fruit's color is = " + color); // Print fruit color.
     }
 }
+
+// Main class Myclass to demonstrate the usage of Fruit class.
 public class Myclass {
     public static void main(String[] args) {
-        Fruit f1 = new Fruit();  // object create
-        f1.name = "Apple";
-        f1.color = "Red";
-        f1.show(); // method call using object
+        Fruit f1 = new Fruit(); // Create an instance of Fruit class.
+        f1.name = "Apple"; // Assign value to the name attribute of f1.
+        f1.color = "Red"; // Assign value to the color attribute of f1.
+        f1.show(); // Call the show method to display f1's attributes.
 
-        System.out.println();
+        System.out.println(); // Print an empty line for separation.
 
-        Fruit f2 = new Fruit();
-        f2.name = "BANANA";
-        f2.color = "YELLOW";
-        f2.show(); // method call using object
+        Fruit f2 = new Fruit(); // Create another instance of Fruit class.
+        f2.name = "BANANA"; // Assign value to the name attribute of f2.
+        f2.color = "YELLOW"; // Assign value to the color attribute of f2.
+        f2.show(); // Call the show method to display f2's attributes.
     }
 }
