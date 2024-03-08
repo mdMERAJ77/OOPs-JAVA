a978432147748e5193764c54d41fe77b53a8ed20
MD MERAJ <mmd200975@gmail.com>
2024-03-08 11:28:07 +0530

    Successfully Commit

---
+++ ClassAndObject/src/Demo.java
@@ -0,0 +1,16 @@
+class Test {
+    int a = 1;
+    String b = "MERAJ";
+
+    void show()
+    {
+        System.out.println(a+" "+b);
+    }
+}
+class Demo {
+    public static void main(String[] args) {
+        Test r = new Test();
+        r.show();
+
+    }
+}
