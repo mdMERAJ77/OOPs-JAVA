4e23e46543d810f24b879251e493ecb78e286267
MD MERAJ <mmd200975@gmail.com>
2024-03-10 22:57:13 +0530

    Successfully Commit

---
+++ ClassAndObject/src/ClassPen.java
@@ -0,0 +1,42 @@
+class Pen {
+
+    String color;
+    String type;
+    public void show() {
+        System.out.println("color is "+ color);
+        System.out.println("type is "+type);
+    }
+    public void printColor() {
+        System.out.println(this.color);
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    public void StudentInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+public class ClassPen {
+    public static void main(String[] args)
+    {
+        Student s1 = new Student();
+        s1.name= "MERAJ";
+        s1.age = 20;
+
+        s1.StudentInfo();
+
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        Pen pen2 = new Pen();
+        pen2.color = "black";
+        pen2.type = "ballpen";
+
+        pen1.printColor();
+        pen2.printColor();
+    }
+}
